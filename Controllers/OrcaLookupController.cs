cb4a38c5320f2f308fed4768a9752a10f64eb93c
John Doherty <contact@johndoherty.info>
2020-10-07 19:23:49 +0100

    initial commit

---
+++ Controllers/OrcaLookupController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrcaLookupDotNet.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    [Produces("application/json")]
+    public class OrcaLookupController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // get the incoming barcode from Orca Scan
+            string barcode = HttpContext.Request.Query["barcode"].ToString();
+            
+            // go get the data from a database
+
+            // return it as a JSON object with 200 response
+            return new OkObjectResult(new { Id = 123, barcode = barcode, Name = "Hero" });
+        }
+    }
+}
